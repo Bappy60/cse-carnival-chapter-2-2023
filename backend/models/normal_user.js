7dc26c5e27848f8a30cf8ab74f28a05fef43fa4d
Bappy60 <kawsar.bappy@vivacomsolutions.com>
2023-10-14 16:43:11 +0600

    normal_user model added

---
+++ backend/models/normal_user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const normal_user = new Schema({
+   email:{
+    require:true,
+     type:String,
+   },
+   password:{
+    require:true,
+     type:String,
+   },
+ firstName:String,
+ lastName:String,
+ dob:String,
+ images:String,
+ country:String,
+ profileBio:String,
+ languages:Array,
+ sponsoredHours:Number,
+ paymentInfo:String,
+ onboardDate:String,
+ hours:Number,
+ valid:String,
+});
+
+const normal_users = mongoose.model('normal_users', normal_user);
+
+module.exports = normal_user;
