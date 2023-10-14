b5ad13afa3b07f54315e53fcd79b74702eb42a4b
Bappy60 <kawsar.bappy@vivacomsolutions.com>
2023-10-14 15:29:47 +0600

    db connection established

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+var bodyParser = require('body-parser');
+const dotenv=require("dotenv");
+const cors = require("cors");
+
+
 const app = express();
+app.use(cors());
+
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.json());
+
+dotenv.config({path:"./config.env"});
+require("./db/conn.js");
 
 app.listen("5000", () => {
     console.log("Server is running!");
